af62382d2d7948cc6bb41e01f85831f4af94a8f3
Kyle Terry <kyle@kyleterry.com>
2011-11-17 18:50:26 -0800

    Telegram API fixes

--- telegram/api.py
+++ telegram/api.py
@@ -1,6 +1,7 @@
 from telegram.models import (Telegram, SendLog, Channel, LEVEL_CHOICES,
         Subscription)
 from telegram.exceptions import ChannelDoesNotExist, LevelDoesNotExist
+from telegram.utils import import_handler
 
 
 def send_telegram(channel, subject, message, level):
@@ -8,16 +9,17 @@ def send_telegram(channel, subject, message, level):
         channel = Channel.objects.get(name=channel)
     except Channel.DoesNotExist:
         raise ChannelDoesNotExist('Channel %s does not exist. Cannot proceed'
-                ' with sending the message.')
+                ' with sending the message.' % channel)
     existing_level = None
     for lvl in LEVEL_CHOICES:
-        if lvl[0] == level:
+        if lvl[0] == level.upper():
             existing_level = level
+            break
         else:
             continue
     if not existing_level:
         raise LevelDoesNotExist('Level %s does not exist. Cannot proceed'
-                ' with sending the message.')
+                ' with sending the message.' % level)
     return _build_telegrams(channel, subject, message, existing_level)
 
 
@@ -38,4 +40,6 @@ def _build_telegrams(channel, subject, message, level):
 def send_all_unsent_telegrams():
     send_logs = SendLog.objects.filter(sent=False)
     for send_log in send_logs:
-        handler = 'a'
+        platform = send_log.subscription_platform.platform
+        subscription = send_log.subscription_platform.subscription
+        handler = import_handler(platform.handler)
